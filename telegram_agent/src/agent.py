446513d4a6a074bc69aa18c85a53be065b43519c
philogicae <38438271+philogicae@users.noreply.github.com>
2025-06-15 14:40:25 +0300

    feat: add Ollama LLM support and improve agent output formatting

--- telegram_agent/src/agent.py
+++ telegram_agent/src/agent.py
@@ -2,59 +2,109 @@ from os import getenv, path
 from typing import Any
 
 from dotenv import load_dotenv
+from langchain_core.language_models import LanguageModelLike
 from langchain_core.messages import HumanMessage
 from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain_ollama import ChatOllama
 from langchain_openai import ChatOpenAI
 from langgraph.prebuilt import create_react_agent
-from pyjson5 import load
+from pyjson5 import load  # pylint: disable=no-name-in-module
+from rich import print as pr
 
 load_dotenv()
-BASE_URL = getenv("OPENAI_API_BASE")
-API_KEY: Any = getenv("OPENAI_API_KEY")
-MODEL = getenv("OPENAI_API_MODEL")
-if not BASE_URL:
-    raise ValueError("OPENAI_API_BASE environment variable is not set")
-if not API_KEY:
-    raise ValueError("OPENAI_API_KEY environment variable is not set")
-if not MODEL:
-    raise ValueError("OPENAI_API_MODEL environment variable is not set")
 
-llm = ChatOpenAI(
-    base_url=BASE_URL, api_key=API_KEY, model=MODEL, disable_streaming="tool_calling"
-)
 
-config_file = path.join(
-    path.dirname(path.dirname(path.dirname(__file__))), "mcp_config.json"
-)
-if not path.exists(config_file):
-    print("mcp_config.json file not found: creating a empty one")
-    with open(config_file, "w", encoding="utf-8") as f:
-        f.write("{}")
-config = load(open(config_file, "r", encoding="utf-8"))
-# TODO: Change config structure
+def get_config() -> Any:
+    config_file = path.join(  # TODO: Change config structure
+        path.dirname(path.dirname(path.dirname(__file__))), "mcp_config.json"
+    )
+    if not path.exists(config_file):
+        print("mcp_config.json file not found: creating a empty one")
+        with open(config_file, "w", encoding="utf-8") as f:
+            f.write("{}")
+    return load(open(config_file, "r", encoding="utf-8"))
+
+
+def get_llm() -> LanguageModelLike:
+    return (
+        ChatOllama(
+            base_url=getenv("OLLAMA_API_BASE"),
+            model=getenv("OLLAMA_API_MODEL"),  # type: ignore
+            disable_streaming=True,
+            num_thread=1,
+            temperature=0.6,
+            top_p=0.95,
+            top_k=20,
+            num_ctx=6144,  # 2Go VRAM APU
+            num_predict=2048,
+        )
+        if getenv("LLM_CHOICE") == "ollama"
+        else ChatOpenAI(
+            base_url=getenv("OPENAI_API_BASE"),
+            api_key=getenv("OPENAI_API_KEY"),  # type: ignore
+            model=getenv("OPENAI_API_MODEL"),  # type: ignore
+            disable_streaming=True,
+        )
+    )
 
 
 async def run_agent() -> None:
-    client = MultiServerMCPClient(config)
-    tools = await client.get_tools()
-    agent = create_react_agent(llm, tools)
-    # TODO: Change system prompts
+    tools = await MultiServerMCPClient(get_config()).get_tools()
+    agent = create_react_agent(
+        name="General Agent",
+        model=get_llm(),
+        tools=tools,
+        prompt=None,  # TODO: Change system prompts
+        checkpointer=None,
+        store=None,
+        debug=False,
+    )
+
+    # user_input = "Using think tool, give me a smart strategy to get rich in 10 years"
+    user_input = "Find torrent for: berserk"
     while True:
         try:
-            user_input = input("Prompt: ")
-            print("-------------------")
+            print("------------ USERS ------------")
+            if user_input:
+                print(f"> {user_input}")
+            user_input = user_input or input("> ")
             if user_input.lower() == "exit":
                 break
             async for chunk in agent.astream(
-                {"messages": [HumanMessage(content=user_input)]}, config
+                {"messages": [HumanMessage(content=user_input)]}
             ):
-                if "agent" in chunk:
-                    msg = chunk["agent"]["messages"][0].content.strip()
-                    if msg:
-                        print(f"------ AGENT ------\n{msg}\n-------------------")
-                elif "tools" in chunk:
-                    msg = chunk["tools"]["messages"][0].content.strip()
-                    if msg:
-                        print(f"------ TOOLS ------\n{msg}\n-------------------")
+                msg_type = "agent"
+                if "tools" in chunk:
+                    msg_type = "tools"
+
+                msg = chunk[msg_type]["messages"][0]
+                think, text = None, None
+                if "<think>" in msg.content:
+                    think, text = msg.content.split("<think>", 1)[1].split(
+                        "</think>", 1
+                    )
+                    think, text = think.strip() or None, text.strip() or None
+                else:
+                    text = msg.content.strip() or None
+
+                tool_calls: list[tuple[str, Any]] | None = None
+                if hasattr(msg, "tool_calls") and msg.tool_calls:
+                    tool_calls = [
+                        (tool.get("name"), tool.get("args")) for tool in msg.tool_calls
+                    ]
+
+                pr(
+                    f"------------ {msg_type.upper()} ------------"
+                    + "\n> think:"
+                    + ("\n" if think else " ")
+                    + f"{think}"
+                    + "\n> text:"
+                    + ("\n" if text else " ")
+                    + f"{text}"
+                    + "\n> tools:"
+                    + ("\n" if tool_calls else " ")
+                    + f"{tool_calls}"
+                )
+                user_input = ""
         except KeyboardInterrupt:
             exit(0)
