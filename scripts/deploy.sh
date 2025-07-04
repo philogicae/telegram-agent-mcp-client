ea67f2f18491eac3e711c038583d1cd7737071af
philogicae <38438271+philogicae@users.noreply.github.com>
2025-07-04 15:56:23 +0300

    fix: update file paths in deploy script and gitignore to match new docker-envs directory

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 source ~/.zshrc
 
-dkr compose -f docker/compose-extended.yaml cp docker/trackers.txt rqbit:/home/rqbit/cache/trackers.txt
-dkr compose -f docker/compose-extended.yaml up -d --build --remove-orphans
+dkr compose -f compose-extended.yaml cp docker-envs/trackers.txt rqbit:/home/rqbit/cache/trackers.txt
+dkr compose -f compose-extended.yaml up -d --build --remove-orphans
