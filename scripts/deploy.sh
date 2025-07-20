b0bb3dade5150c0cc33e55f36e14514c45959522
philogicae <38438271+philogicae@users.noreply.github.com>
2025-07-21 01:40:19 +0300

    feat: improve deployment configuration

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 source ~/.zshrc
 
-dkr compose -f compose-extended.yaml cp docker-envs/trackers.txt rqbit:/home/rqbit/cache/trackers.txt
-dkr compose -f compose-extended.yaml up -d --build --remove-orphans
+dkr compose -f compose.yaml -f extended.yaml cp docker-envs/trackers.txt rqbit:/home/rqbit/cache/trackers.txt
+dkr compose -f compose.yaml -f extended.yaml up -d --build --remove-orphans
