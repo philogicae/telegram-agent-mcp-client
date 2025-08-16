c74e5c67f1802ce0c400868f4a43f85f06255439
philogicae <38438271+philogicae@users.noreply.github.com>
2025-08-16 22:13:14 +0300

    fix: gemini bug + fallback gemini-openai provider

--- telegram_agent/src/core/llm.py
+++ telegram_agent/src/core/llm.py
@@ -41,23 +41,33 @@ def get_llm() -> LanguageModelLike:
             disabled_params={"parallel_tool_calls": False},
             temperature=0.5,
         )
-    if llm_choice == "gemini":
-        return ChatGemini(
-            api_key=getenv("GEMINI_API_KEY"),
-            model=getenv("GEMINI_API_MODEL"),
+    if llm_choice == "openai":
+        model = str(getenv("OPENAI_API_MODEL"))
+        return ChatOpenAI(
+            api_key=getenv("OPENAI_API_KEY"),  # type: ignore
+            model=model,
             disable_streaming="tool_calling",
+            disabled_params={"parallel_tool_calls": False},
+            temperature=0.5 if not model.startswith("gpt-5") else None,
+            reasoning_effort="high" if not model.startswith("gpt-5") else None,
+            output_version="responses/v1",
+        )
+    if llm_choice == "gemini-openai":
+        return ChatOpenAI(
+            base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+            api_key=getenv("GEMINI_API_KEY"),  # type: ignore
+            model=getenv("GEMINI_API_MODEL"),  # type: ignore
+            disable_streaming="tool_calling",
+            disabled_params={"parallel_tool_calls": False},
             temperature=0.5,
-            thinking_budget=1024,  # -1 for dynamic/unlimited
-            # include_thoughts=True,
+            reasoning_effort="low",  # low=1024, medium=8192, high=24576
         )
 
-    model = getenv("OPENAI_API_MODEL")
-    return ChatOpenAI(
-        api_key=getenv("OPENAI_API_KEY"),  # type: ignore
-        model=model,  # type: ignore
+    return ChatGemini(
+        api_key=getenv("GEMINI_API_KEY"),
+        model=getenv("GEMINI_API_MODEL"),
         disable_streaming="tool_calling",
-        disabled_params={"parallel_tool_calls": False},
-        temperature=0.5 if not str(model).startswith("gpt-5") else None,
-        reasoning_effort="high" if not str(model).startswith("gpt-5") else None,
-        output_version="responses/v1",
+        temperature=0.5,
+        thinking_budget=1024,  # -1 for dynamic/unlimited
+        transport="rest",  # Avoid retry bug
     )
