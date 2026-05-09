4b3cb1e02ee5b184fb0249bd94ba26bd793dc943
philogicae <38438271+philogicae@users.noreply.github.com>
2026-05-09 20:41:11 +0300

    chore: update Fireworks model to kimi-k2p6-turbo and suppress LangChain deprecation warnings

--- telegram_agent/patch_pydantic_v1.py
+++ telegram_agent/patch_pydantic_v1.py
@@ -2,7 +2,13 @@
 
 from warnings import filterwarnings
 
+from langchain_core._api.deprecation import LangChainPendingDeprecationWarning
+
 # Suppress Pydantic V1 compatibility warning on Python 3.14+
 filterwarnings("ignore", category=UserWarning)
+filterwarnings(
+    "ignore",
+    category=LangChainPendingDeprecationWarning,
+)
 
 _PYDANTIC_V1_ANCHOR = None
